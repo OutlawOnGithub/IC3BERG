4769a81de1acf616d1fb63ab17d1bba62c308076
OutlawOnGithub <lucaspaillatpro@proton.me>
2023-11-16 00:25:30 +0100

    handled the no new things to fetch case
    with a basic database, I checked if the feed was already know or not

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ def main():
 
     rss = RSS()
 
-    feed_urls = ['https://krebsonsecurity.com/feed/','http://www.bleepingcomputer.com/feed/']
+    feed_dict = {
+        'https://krebsonsecurity.com/feed/' : '',
+        'http://www.bleepingcomputer.com/feed/' : '',
+    }
 
     channel_name = 'infosec'
     previous_entry_id_per_server = {}
@@ -29,38 +32,42 @@ def main():
 
     @tasks.loop(seconds = 20) # repeat after every 5 minutes
     async def fetch_feeds():
-        for feed in feed_urls:
-            news_feed = feedparser.parse(feed)
-            # if latest_id != news_feed.entries[0]['id']: #test si c'est nouveau 
-            embed = discord.Embed(
-                title=news_feed.entries[0]['title'],
-                url=news_feed.entries[0]['link'],
-                description=news_feed.entries[0]['summary'],
-                color=discord.Color.blue()
-            )
-            embed.set_author(name=news_feed.entries[0]['author'])
-            embed.set_footer(text=f"{datetime.datetime.utcnow()}")
-            
-            latest_id = news_feed.entries[0]['id']
-            for guild in bot.guilds:
-                channel = discord.utils.get(guild.channels, name='infosec', type=discord.ChannelType.text)
-                if channel:
-                    print('sent shit')
-                    await channel.send(embed=embed)
-                else:
-                    print('fetched already')
+        print(feed_dict)
+        for feed_url in feed_dict.keys():
+
+            news_feed = feedparser.parse(feed_url)
+            if feed_dict[feed_url] != news_feed.entries[0]['id']: #test si c'est nouveau 
+                embed = discord.Embed(
+                    title=news_feed.entries[0]['title'],
+                    url=news_feed.entries[0]['link'],
+                    description=news_feed.entries[0]['summary'],
+                    color=discord.Color.blue()
+                )
+                embed.set_author(name=news_feed.entries[0]['author'])
+                embed.set_footer(text=f"{datetime.datetime.utcnow()}")
+                
+                for guild in bot.guilds:
+                    channel = discord.utils.get(guild.channels, name='infosec', type=discord.ChannelType.text)
+                    if channel:
+                        await channel.send(embed=embed)
+                    else:
+                        print('fetched already')
+            feed_dict[feed_url] = news_feed.entries[0]['id']
+            print(feed_dict)
 
     @bot.command(name='startfeeds')
     async def start_feeds(ctx):
         guild_id = ctx.guild.id
         fetching_status_per_server[guild_id] = True
         if fetch_feeds.is_running():
+            print('alreasdy running')
             fetch_feeds.cancel()
             asyncio.sleep(3)
             fetch_feeds.start()
         else:
+
             fetch_feeds.start()
-        print(f"Fetching started for server {ctx.guild.name}...")
+        print(f"Fetching started for all servers")#server {ctx.guild.name}...")
         await ctx.send('RSS feed updates will now be fetched every 5 minutes.')
 
     @bot.command(name='stopfeeds')
