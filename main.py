c0c7c7cd1a5e9f0e20f1bcfcb9bdf4c414b7a13d
OutlawOnGithub <lucaspaillatpro@proton.me>
2023-12-28 13:31:54 +0100

    back to normal code + logging

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ def main():
         'http://www.bleepingcomputer.com/feed/' : '',
     }
 
-    #handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-
     channel_name = 'infosec'
     fetching_status_per_server = {}
 
@@ -32,32 +30,35 @@ def main():
 
     @tasks.loop(minutes = 5) # repeat after every 5 minutes
     async def fetch_feeds():
+        for guild in bot.guilds: #added for debug
+            channel = discord.utils.get(guild.channels, name=channel_name, type=discord.ChannelType.text)
+            if channel:
+                await channel.send("Bro I'm fetching")
         #print(feed_dict)
-        print(f"I am fetching !")
-    #     for feed_url in feed_dict.keys():
+        # for feed_url in feed_dict.keys():
 
-    #         news_feed = feedparser.parse(feed_url)
-    #         if feed_dict[feed_url] != news_feed.entries[0]['id']: #test si c'est nouveau 
-    #             embed = discord.Embed(
-    #                 title=news_feed.entries[0]['title'],
-    #                 url=news_feed.entries[0]['link'],
-    #                 description=news_feed.entries[0]['description'],
-    #                 color=discord.Color.blue()
-    #             )
-    #             # if 'img' in news_feed.entries[0]:
-    #             #     embed.set_image(url=news_feed.entries[0]['img']['url'])
-    #             embed.set_author(name=news_feed.entries[0]['author'])
-    #             current_time_gmt = datetime.now(timezone.utc)
-    #             formatted_time = current_time_gmt.strftime("%H:%M:%S %d/%m/%Y %Z")
-    #             embed.set_footer(text=f"{formatted_time}")
+        #     news_feed = feedparser.parse(feed_url)
+        #     if feed_dict[feed_url] != news_feed.entries[0]['id']: #test si c'est nouveau 
+        #         embed = discord.Embed(
+        #             title=news_feed.entries[0]['title'],
+        #             url=news_feed.entries[0]['link'],
+        #             description=news_feed.entries[0]['description'],
+        #             color=discord.Color.blue()
+        #         )
+        #         # if 'img' in news_feed.entries[0]:
+        #         #     embed.set_image(url=news_feed.entries[0]['img']['url'])
+        #         embed.set_author(name=news_feed.entries[0]['author'])
+        #         current_time_gmt = datetime.now(timezone.utc)
+        #         formatted_time = current_time_gmt.strftime("%H:%M:%S %d/%m/%Y %Z")
+        #         embed.set_footer(text=f"{formatted_time}")
                 
-    #             for guild in bot.guilds:
-    #                 channel = discord.utils.get(guild.channels, name=channel_name, type=discord.ChannelType.text)
-    #                 if channel:
-    #                     await channel.send(embed=embed)
-    #                 else:
-    #                     print('fetched already')
-    #         feed_dict[feed_url] = news_feed.entries[0]['id']
+        #         for guild in bot.guilds:
+        #             channel = discord.utils.get(guild.channels, name=channel_name, type=discord.ChannelType.text)
+        #             if channel:
+        #                 await channel.send(embed=embed)
+        #             else:
+        #                 print('fetched already')
+        #     feed_dict[feed_url] = news_feed.entries[0]['id']
 
 
     @bot.command(name='startrss')
@@ -65,10 +66,10 @@ def main():
         guild_id = ctx.guild.id
         fetching_status_per_server[guild_id] = True
         if not fetch_feeds.is_running():
-            fetch_feeds.start()
+            #fetch_feeds.start()
             #print(f"Fetching started for all servers")#server {ctx.guild.name}...")
-            print(f"Fetching - Started - {ctx.guild}")
-            await ctx.send('RSS feed updates will now be fetched every 5 minutes.')
+            #await ctx.send('RSS feed updates will now be fetched every 5 minutes.')
+            await ctx.send('RSS feeds are disabled until we fix the lag issue')
         else:
             await ctx.send('RSS feed updates are already being fetched.')
 
@@ -78,9 +79,10 @@ def main():
         guild_id = ctx.guild.id
         if fetch_feeds.is_running():
             fetching_status_per_server[guild_id] = False
-            print(f"Fetching - Stopped - {ctx.guild}")
+            print(f"Fetching stopped for all servers.")
             fetch_feeds.cancel()
             #await ctx.send('RSS feed updates fetching has been stopped.')
+            await ctx.send('RSS feeds are disabled until we fix the lag issue')
         else:
             fetch_feeds.cancel()
             await ctx.send('The bot is not currently fetching.')
@@ -91,10 +93,8 @@ def main():
         if feed_url:
             print(f"Added feed : {feed_url}")
             feed_dict[feed_url] = ''
-            print(f"AddRSS - Success - {feed_url} - {ctx.guild}")
             await ctx.send(f'You successfully added the RSS feed : `{feed_url}`')
         else:
-            print(f"AddRSS - Fail - {ctx.guild}")
             await ctx.send(f'A feed url is required')
       
 
@@ -102,10 +102,11 @@ def main():
     async def status(ctx):
         #guild_id = ctx.guild.id
         if fetch_feeds.is_running():
-            print(f"Status - Fetching - {ctx.guild}")
-            await ctx.send(f'The bot is currently fetching RSS feed updates in all servers.')
+            print("Status : Fetching")
+            #await ctx.send(f'The bot is currently fetching RSS feed updates in all servers.')
+            await ctx.send('RSS feeds are disabled until we fix the lag issue')
         else:
-            print(f"Status - Not fetching - {ctx.guild}")
+            print("Status : Not fetching")
             await ctx.send(f'The bot is not fetching RSS feed updates in all servers.')
 
 
@@ -124,7 +125,6 @@ def main():
         embed.add_field(name='_help', value='Displays this help message', inline=False)
         embed.set_footer(text='For any requests, DM `ox6cfc1ab7`')
 
-        print(f"Help - {ctx.guild}")
         await ctx.send(embed=embed)
 
 
@@ -138,33 +138,10 @@ def main():
                                    The project will be opensourced on Github once we deploy the v1.0.0""",
                     color=discord.Color.blue()
                 )
-        print(f"Info - {ctx.guild}")
         await ctx.send(embed=embed)
-
-    
-    @bot.command(name='check_rate_limit')
-    async def check_rate_limit(ctx):
-        # Make a request to the Discord API (example: fetching the bot's own information)
-        user = await bot.http.get_me()
-
-        # Access the response headers to get rate limit information
-        rate_limit_remaining = user.headers.get('X-RateLimit-Remaining')
-        rate_limit_reset = user.headers.get('X-RateLimit-Reset')
-
-        if rate_limit_remaining is not None:
-            response_message = (
-                f'Remaining requests: {rate_limit_remaining}\n'
-                f'Reset time: {rate_limit_reset}'
-            )
-            await ctx.send(response_message)
-        else:
-            await ctx.send('Rate limit information not available for this request.')
-
-
 
 
     bot.run(TOKEN, log_level=logging.DEBUG)
-    #, log_handler=handler, log_level=logging.DEBUG)
- 
+
 if __name__ == "__main__":
     main()
