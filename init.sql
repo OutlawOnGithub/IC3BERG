ea15d7b9e1bd2e151b8275cd11c1067a5f06eced
Grégory MOU KUI <gmoukui@piwwop.com>
2024-04-03 12:08:44 +0200

    Refacto: Only start DB with compose

--- init.sql
+++ init.sql
@@ -15,3 +15,21 @@ CREATE TABLE IF NOT EXISTS iceberg.rss (
     description TEXT,
     guild_id BIGINT
 );
+
+-- Create schema if not exists
+CREATE SCHEMA IF NOT EXISTS iceburk;
+
+-- Create server table
+CREATE TABLE IF NOT EXISTS iceburk.server (
+    guild_id BIGINT PRIMARY KEY,
+    enabled BOOLEAN,
+    rss_channel BIGINT
+);
+
+-- Create rss table
+CREATE TABLE IF NOT EXISTS iceburk.rss (
+    url TEXT PRIMARY KEY,
+    last_fetched TEXT,
+    description TEXT,
+    guild_id BIGINT
+);
