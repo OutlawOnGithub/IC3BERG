63152e31a841a18cd23ff2dd5aa815f953a3f48d
Grégory MOU KUI <firzamdigidix@gmail.com>
2024-04-09 09:30:00 +0200

    Hotfix typo

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import psycopg2
 def main():
     TOKEN = os.getenv("DISCORD_TOKEN")
     DB_PW = os.getenv("POSTGRES_PASSWORD")
-    PREFIX = os.getenv("PREFIX")
+    PREFIX = os.getenv("BOT_PREFIX")
 
     bot = commands.Bot(
         command_prefix=PREFIX,
